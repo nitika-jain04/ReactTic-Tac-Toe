e1a786049fcc92f3a248cf439fb4322931158b81
Nitika Jain <nitikaaajain.04@gmail.com>
2023-10-27 16:37:50 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import Player from "./assets/Components/Player";
+import GameBoard from "./assets/Components/GameBoard";
+
+function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        ></GameBoard>
+      </div>
+    </main>
+  );
+}
+export default App;
---
+++ src/assets/Components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick(params) {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    // console.log(event);
+    setPlayerName(event.target.value); //value entered in input field
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span>
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
